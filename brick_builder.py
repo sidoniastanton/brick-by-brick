1e253ec6fd53cfe6f7fe424b72b3d0782ad7825c
sidoniastanton <sidonia.stanton@maine.edu>
2024-10-07 10:12:56 -0400

    commit 3
    finally got the pyramid

--- brick_builder.py
+++ brick_builder.py
@@ -22,6 +22,7 @@ Sidonia Stanton
 '''
 userInput = int(input('Please enter a number: '))
 
+'''
 # standard triangle
 for row in range(userInput):
     for col in range(row + 1):
@@ -56,3 +57,17 @@ for row in range(userInput):
     print()
 
 print()
+'''
+
+#   F I X
+# pyramid - WIP - use multiple loops - x increase by 2 each time - dont have to include spaces on right
+for row in range(userInput): # for each horiz row in the range of user input # -- establush number of rows
+    for col in range(userInput - row - 1): # for each row - for each column of O's print single o
+        print(" ", end='')
+    for col in range(row * 2 + 1): # for each column of x print single x
+        print ("x", end='')
+    print()
+
+#  add 2 every row - row * 2 + 1
+print()
+
